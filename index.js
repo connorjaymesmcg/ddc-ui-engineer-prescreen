1447d4873efc7aff5b672abe900aa30295610271
Connor McGinnis <connorjaymesmcg@gmail.com>
2021-07-05 10:00:42 -0400

    new change: splitting textarea into JSON object

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// Establish DOM elements 
 const inputTextArea = document.querySelector('.prescreen-input');
 const outputTextArea = document.querySelector('.prescreen-output');
 const totalButton = document.querySelector('.prescreen-button');
 
-inputTextArea.value =
+inputTextArea.innerHTML =
   `
 John: 2
 Jane: 3
@@ -11,29 +12,13 @@ Jane: 5
 `;
 
 
-
+// Click listener for totaling sums of keys
 totalButton.addEventListener('click', () => {
-  const newText = inputTextArea.value.split(/[\n]/).filter(element => element != '').map(element => element.trim());
+  // const newText = inputTextArea.value.split(/[\n]/).filter(element => element != '').map(element => element.trim());
+  // console.log(newText);
+  console.log(inputTextArea.innerHTML);
+  let newText = inputTextArea.value.split(/[\n]/).filter(element => element != '');
   console.log(newText);
-  const buildObject = arr => {
-    const obj = {};
-    for (el of arr) {
-      let [a, b] = el.split(':');
-      console.log(parseInt(b) + parseInt(b));
-      obj[a] = b;
-      console.log(obj);
-    }
-    return obj;
 
-    // let arrayObj = [];
-    // for (let i = 0; i < arr.length; i += 2) {
-    //   const obj = {};
-    //   obj[arr[i]] = arr[i + 1];
-    //   arrayObj.push(obj);
-    // }
-    // console.log(Object.values(arrayObj))
-    // return arrayObj;
-  };
 
-  console.log(buildObject(newText));
 });
