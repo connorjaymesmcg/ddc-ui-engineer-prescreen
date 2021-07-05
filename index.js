e1e9c52f8a631e534b852fbaabeb6c2f006f008e
Connor McGinnis <connorjaymesmcg@gmail.com>
2021-07-05 11:58:30 -0400

    working

--- index.js
+++ index.js
@@ -17,8 +17,28 @@ totalButton.addEventListener('click', () => {
   // const newText = inputTextArea.value.split(/[\n]/).filter(element => element != '').map(element => element.trim());
   // console.log(newText);
   console.log(inputTextArea.innerHTML);
-  let newText = inputTextArea.value.split(/[\n]/).filter(element => element != '');
-  console.log(newText);
+  // let array = inputTextArea.value.split(/[\n]/).filter(element => element != '');
+  let inputArray = inputTextArea.value.trim().split(/\n/)
+  let newTextString = JSON.stringify(array);
+  let nameParser = newTextString.match(/(?<=")(\w+)(?=:)/gi);
+  let uniqueNameKeys = [...new Set(nameParser)];
 
+  const buildUserObject = arr => {
+    let obj = {};
+    for (el of arr) {
+      obj[el] = 0;
+    }
+    return obj;
+  };
 
+  let usersObject = buildUserObject(uniqueNameKeys);
+
+  inputArray.forEach((el) => {
+    let name = el.match(/([A-Z])+(?=:)/gi);
+    let number = el.match(/(\d)+/gi);
+    usersObject[name] = usersObject[name] + parseInt(number);
+    console.log(usersObject[name]);
+  });
+
+  console.log("Object:", usersObject);
 });
